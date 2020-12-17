77f018524729f99b863a3a8d9b151b5d90dcf5cb
Julia <nuwanda57@yandex.ru>
2020-12-17 22:21:01 +0300

    tt

--- formulas_vae/experiments.py
+++ formulas_vae/experiments.py
@@ -7,6 +7,7 @@ import formulas_vae.vocab as my_vocab
 import formulas_vae.utils as my_utils
 import formulas_vae.model as my_model
 import formulas_vae.train as my_train
+import formulas_vae.train_best_worst as my_train_best_worst
 
 import results.analyse_results as my_analyse_results
 
@@ -35,6 +36,34 @@ def reconstruct_test_based_on_epoch(
         model.to(device)
         optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=betas)
         my_train.train(vocab, model, optimizer, train_batches, valid_batches, epochs)
+        model.reconstruct(test_batches, test_order, max_len, rec_file_template % epochs, strategy=reconstruct_strategy)
+
+
+def reconstruct_test_based_on_epoch_tmp(
+        train_file, val_file, test_file, reconstruct_strategy, max_len, epochs_list, results_dir,
+        model_conf_params, batch_size=256, lr=0.0005, betas=(0.5, 0.999)):
+    if not os.path.exists(results_dir):
+        os.mkdir(results_dir)
+
+    vocab = my_vocab.Vocab()
+    vocab.build_from_formula_file(train_file)
+    vocab.write_vocab_to_file(os.path.join(results_dir, 'vocab.txt'))
+    device = torch.device('cuda')
+    valid_batches, _ = my_utils.build_ordered_batches(val_file, vocab, batch_size, device)
+    test_batches, test_order = my_utils.build_ordered_batches(test_file, vocab, batch_size, device)
+    rec_file_template = os.path.join(results_dir, 'rec_%d')
+
+    for epochs in epochs_list:
+        if os.path.exists(rec_file_template % epochs):
+            print('WARNING: rec file already exists, skipping epochs %d' % epochs)
+            continue
+        model_params = my_model.ModelParams(vocab=vocab, vocab_size=vocab.size(), device=device, **model_conf_params)
+        model = my_model.ExtendedFormulaVARE(model_params)
+        model.to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=betas)
+        my_train_best_worst.train(
+            vocab, model, optimizer, train_file, valid_batches, epochs, batch_size, max_len, device,
+            log_interval=20, update_train_epochs=20, training_log_dir='training/', choose_worst=True)
         model.reconstruct(test_batches, test_order, max_len, rec_file_template % epochs, strategy=reconstruct_strategy)
 
 
---
+++ formulas_vae/train_best_worst.py
@@ -0,0 +1,96 @@
+import torch
+import random
+import torch.nn.functional as F
+import numpy as np
+
+import os
+
+import formulas_vae.utils as my_utils
+
+
+# Reconstruction error + KL divergence
+def loss_function(logits, targets, mu, logsigma, vocab):
+    reconstruction_loss = F.cross_entropy(
+        logits.view(-1, logits.size(-1)), targets.view(-1),
+        ignore_index=vocab.pad_token_index, reduction='none').view(targets.size())
+    KLD = -0.5 * torch.sum(1 + logsigma - mu.pow(2) - logsigma.exp()) / len(mu)
+    # reconstruction_loss: (formula_dim, batch_size), so we take sum over all torens and mean over formulas in batch
+    return reconstruction_loss.sum(dim=0).mean(), KLD
+
+
+def evaluate(model, batches, vocab):
+    model.eval()
+    kl_losses, rec_losses = [], []
+    with torch.no_grad():
+        for inputs, targets in batches:
+            logits, mu, logsigma, z = model(inputs)
+            rec, kl = loss_function(logits, targets, mu, logsigma, vocab)
+            kl_losses.append(kl.item())
+            rec_losses.append(rec.item())
+    loss = np.mean(rec_losses)
+    return loss, np.mean(rec_losses), np.mean(kl_losses)
+
+
+def update_train_dataset(old_train_path, new_train_path, vocab, model, batch_size, device, max_len,
+                         xs, training_dir, choose_worst, fraction):
+    train_batches, train_order = my_utils.build_ordered_batches(old_train_path, vocab, batch_size, device)
+    rec_path = os.path.join(training_dir, 'rec')
+    model.reconstruct(train_batches, train_order, max_len, rec_path, strategy='mu')
+    mses = my_utils.reconstruction_mses(rec_path, old_train_path, xs)
+
+    mse_with_inds = list(enumerate(mses))
+    mse_with_inds.sort(key=lambda k: k[1], reverse=choose_worst)
+
+    new_formula_indices = set([m[0] for m in mse_with_inds[:(len(mse_with_inds) * fraction)]])
+    written = 0
+    with open(old_train_path) as old_file, open(new_train_path, 'w') as new_file:
+        for i, line in enumerate(old_file):
+            if i in new_formula_indices:
+                new_file.write(line)
+                written += 1
+                if written != len(new_formula_indices):
+                    new_file.write('\n')
+
+
+def train(vocab, model, optimizer, train_file_path, valid_batches, epochs, batch_size, max_len, device,
+          log_interval=20, update_train_epochs=20, training_log_dir='training/', choose_worst=True):
+
+    xs = np.linspace(0.0, 1.0, num=100)
+    if not os.path.exists(training_log_dir):
+        os.mkdir(training_log_dir)
+
+    cur_train_path = train_file_path
+    train_batches, _ = my_utils.build_ordered_batches(cur_train_path, vocab, batch_size, device)
+    for epoch in range(epochs):
+        print('Epoch %d' % (epoch + 1))
+        kl_losses, rec_losses, losses = [], [], []
+        model.train()
+        indices = list(range(len(train_batches)))
+        random.shuffle(indices)
+        for i, idx in enumerate(indices):
+            optimizer.zero_grad()
+            inputs, targets = train_batches[idx]
+            logits, mu, logsigma, z = model(inputs)
+            rec, kl = loss_function(logits, targets, mu, logsigma, vocab)
+            loss = rec
+            loss.backward()
+            optimizer.step()
+            rec_losses.append(rec.item())
+            losses.append(loss.item())
+            kl_losses.append(kl.item())
+
+            if (i + 1) % log_interval == 0:
+                print('\t[training] %d/%d' % (i + 1, len(indices)), end=' ')
+                print('loss: %0.3f, rec loss: %0.3f, kl: %0.3f' % (
+                    np.mean(losses), np.mean(rec_losses), np.mean(kl_losses)))
+
+            if (i + 1) % update_train_epochs == 0:
+                print('updating train dataset')
+                new_train_path = os.path.join(training_log_dir, 'train_%d.txt' % (i + 1))
+                update_train_dataset(cur_train_path, new_train_path, vocab, model, batch_size, device, max_len,
+                                     xs, training_log_dir, choose_worst, 0.9)
+                cur_train_path = new_train_path
+                train_batches, _ = my_utils.build_ordered_batches(cur_train_path, vocab, batch_size, device)
+
+        valid_losses = evaluate(model, valid_batches, vocab)
+        print('\t[validation] loss: %0.3f, rec loss: %0.3f, kl: %0.3f' % valid_losses)
