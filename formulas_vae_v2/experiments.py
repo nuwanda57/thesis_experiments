35088e016fef49bd9ec243f0ac4fcb97d27813b7
Julia <nuwanda57@yandex.ru>
2021-01-21 23:03:35 +0300

    tt

--- formulas_vae_v2/experiments.py
+++ formulas_vae_v2/experiments.py
@@ -9,6 +9,8 @@ import formulas_vae_v2.train as my_train
 import formulas_vae_v2.batch_builder as my_batch_builder
 import formulas_vae_v2.generative_train as my_generative_train
 
+import wandb
+
 
 def reconstruct_test_per_epoch(
         train_file, val_file, test_file, reconstruct_strategy, max_len, epochs, results_dir,
@@ -40,7 +42,8 @@ def reconstruct_test_per_epoch(
 
 
 def exp_generative_train(train_file, val_file, test_file, reconstruct_strategy, max_len, epochs, results_dir,
-        model_conf_params, reconstruct_frequency=50, batch_size=256, lr=0.0005, betas=(0.5, 0.999)):
+        model_conf_params, batch_size=256, lr=0.0005, betas=(0.5, 0.999)):
+    wandb.init(project="generative train")
     if not os.path.exists(results_dir):
         os.mkdir(results_dir)
     training_log_dir = os.path.join(results_dir, 'training/')
@@ -53,12 +56,24 @@ def exp_generative_train(train_file, val_file, test_file, reconstruct_strategy, 
     device = torch.device('cuda')
     train_batches, _ = my_batch_builder.build_ordered_batches(train_file, vocab, batch_size, device)
     valid_batches, _ = my_batch_builder.build_ordered_batches(val_file, vocab, batch_size, device)
-    test_batches, test_order = my_batch_builder.build_ordered_batches(test_file, vocab, batch_size, device)
-    rec_file_template = os.path.join(results_dir, 'rec_%d')
 
     model_params = my_model.ModelParams(vocab=vocab, vocab_size=vocab.size(), device=device, **model_conf_params)
     model = my_model.FormulaVARE(model_params)
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=betas)
+    n_formulas_to_sample = 2000
+    use_for_train_fraction = 0.2
+    n_pretrain_steps = 250
+    wandb_log = {
+        'max_len': max_len,
+        'epochs': epochs,
+        'batch_size': batch_size,
+        'learning_rate': lr,
+        'n_formulas_sampled': n_formulas_to_sample,
+        'chosen_for_train_fraction': use_for_train_fraction,
+        'n_pretrain_steps': n_pretrain_steps,
+    }
+    wandb.log(wandb_log)
     my_generative_train.generative_train(model, vocab, optimizer, epochs, device, batch_size,
-                                         2000, 'sample', max_len, 0.2, 250, train_batches, valid_batches)
+                                         n_formulas_to_sample, 'sample', max_len, use_for_train_fraction,
+                                         n_pretrain_steps, train_batches, valid_batches)
--- formulas_vae_v2/generative_train.py
+++ formulas_vae_v2/generative_train.py
@@ -1,12 +1,11 @@
-import torch
-import random
-import torch.nn.functional as F
 import numpy as np
 from sklearn.metrics import mean_squared_error
 
 import formulas_vae_v2.train as my_train
 import formulas_vae_v2.evaluate_formula as my_evaluate_formula
 import formulas_vae_v2.batch_builder as my_batch_builder
+
+import wandb
 
 
 def generative_train(model, vocab, optimizer, epochs, device, batch_size,
@@ -17,8 +16,11 @@ def generative_train(model, vocab, optimizer, epochs, device, batch_size,
 
     xs = np.linspace(0.0, 1.0, num=100)
     ys = 3 * xs
+    wandb.log({'formuls': '3x'})
     for epoch in range(epochs):
-        model.sample(n_formulas_to_sample, max_length, file_to_sample)
+        wandb_log = {}
+        reconstructed_formulas, _ = model.sample(n_formulas_to_sample, max_length, file_to_sample)
+        wandb_log['example'] = '\n'.join(' '.join(formula) for formula in reconstructed_formulas[:20])
         predicted_ys = my_evaluate_formula.evaluate_file(file_to_sample, xs)
         mses = []
         inf = 10 ** 4
@@ -28,10 +30,15 @@ def generative_train(model, vocab, optimizer, epochs, device, batch_size,
             else:
                 mses.append(mean_squared_error(predicted_ys[i], ys))
         print(f'epoch: {epoch}, mean mses: {np.mean(mses)}')
+        if np.isfinite(np.mean(mses)):
+            wandb_log['mean_mse_generated'] = np.mean(mses)
         best_formula_pairs = sorted(enumerate(mses), key=lambda x: x[1])[:int(len(mses) * use_for_train_fraction)]
         best_formula_pairs = [x for x in best_formula_pairs if x[1] < inf]
         best_formula_mses = [x[1] for x in best_formula_pairs]
         print(f'epoch: {epoch}, mean best mses: {np.mean(best_formula_mses)}')
+        if np.isfinite(np.mean(best_formula_mses)):
+            wandb_log['mean_mse_best_formulas'] = np.mean(best_formula_mses)
+        wandb.log(wandb_log)
         best_formula_indices = [x[0] for x in best_formula_pairs]
         best_formulas = []
         with open(file_to_sample) as f:
