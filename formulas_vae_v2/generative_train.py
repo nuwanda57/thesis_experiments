20afdf5f74f2579c9631ba7b6d74c939f6a2f584
Julia <nuwanda57@yandex.ru>
2021-01-27 19:39:01 +0300

    tt

--- formulas_vae_v2/generative_train.py
+++ formulas_vae_v2/generative_train.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
+from collections import deque
 
 import formulas_vae_v2.train as my_train
 import formulas_vae_v2.evaluate_formula as my_evaluate_formula
@@ -9,8 +10,8 @@ import wandb
 
 
 def generative_train(model, vocab, optimizer, epochs, device, batch_size,
-                     n_formulas_to_sample, file_to_sample, max_length, use_for_train_fraction,
-                     n_pretrain_steps, pretrain_batches, pretrain_val_batches):
+                     n_formulas_to_sample, file_to_sample, max_length, percentile,
+                     n_pretrain_steps, pretrain_batches, pretrain_val_batches, use_n_last_steps=6):
     for step in range(n_pretrain_steps):
         my_train.run_epoch(vocab, model, optimizer, pretrain_batches, pretrain_val_batches, step)
 
@@ -20,6 +21,9 @@ def generative_train(model, vocab, optimizer, epochs, device, batch_size,
     table.add_data('y = 3x')
     wandb.log({'correct formula': table})
     reconstructed_formulas = []
+    best_formulas = []
+    best_mses = []
+    last_best_sizes = deque([0] * use_n_last_steps, maxlen=use_n_last_steps)
     for epoch in range(epochs):
         wandb_log = {}
         reconstructed_formulas, _ = model.sample(n_formulas_to_sample, max_length, file_to_sample)
@@ -39,21 +43,27 @@ def generative_train(model, vocab, optimizer, epochs, device, batch_size,
         print(f'epoch: {epoch}, mean mses: {np.mean(mses)}')
         if np.isfinite(np.mean(mses)) and np.isfinite(np.log(np.mean(mses))):
             wandb_log['log_mean_mse_generated'] = np.log(np.mean(mses))
-        best_formula_pairs = sorted(enumerate(mses), key=lambda x: x[1])[:int(len(mses) * use_for_train_fraction)]
-        best_formula_pairs = [x for x in best_formula_pairs if x[1] < inf]
-        best_formula_mses = [x[1] for x in best_formula_pairs]
-        print(f'epoch: {epoch}, mean best mses: {np.mean(best_formula_mses)}')
-        if np.isfinite(np.mean(best_formula_mses)) and np.isfinite(np.log(np.mean(best_formula_mses))):
-            wandb_log['log_mean_mse_best_formulas'] = np.log(np.mean(best_formula_mses))
-        wandb.log(wandb_log)
-        best_formula_indices = [x[0] for x in best_formula_pairs]
-        best_formulas = []
+        mse_threshold = np.nanpercentile(mses + best_mses, percentile)
+        epoch_best_formula_pairs = [x for x in enumerate(mses) if x[1] < mse_threshold]
+        epoch_best_formula_indices = [x[0] for x in epoch_best_formula_pairs if x[1] < inf]
+        epoch_best_mses = [x[1] for x in epoch_best_formula_pairs if x[1] < inf]
+        print(f'epoch: {epoch}, mean best mses: {np.mean(epoch_best_mses)}')
+        if np.isfinite(np.mean(epoch_best_mses)) and np.isfinite(np.log(np.mean(epoch_best_mses))):
+            wandb_log['log_mean_epoch_mse_best'] = np.log(np.mean(epoch_best_mses))
+        epoch_best_formulas = []
         with open(file_to_sample) as f:
             for i, line in enumerate(f.readlines()):
-                if i in best_formula_indices:
-                    best_formulas.append(line.strip())
+                if i in epoch_best_formula_indices:
+                    epoch_best_formulas.append(line.strip())
+        s = last_best_sizes.popleft()
+        last_best_sizes.append(len(epoch_best_formulas))
+        best_formulas = best_formulas[s:] + epoch_best_formulas
+        best_mses = best_mses[s:] + epoch_best_mses
+        if np.isfinite(np.mean(best_mses)) and np.isfinite(np.log(np.mean(best_mses))):
+            wandb_log['log_mean_mse_best'] = np.log(np.mean(best_mses))
         with open(file_to_sample, 'w') as f:
-            f.write('\n'.join(best_formulas))
+            f.write('\n'.join(epoch_best_formulas))
+        wandb.log(wandb_log)
 
         train_batches, _ = my_batch_builder.build_ordered_batches(file_to_sample, vocab, batch_size, device)
         my_train.run_epoch(vocab, model, optimizer, train_batches, pretrain_val_batches, epoch)
