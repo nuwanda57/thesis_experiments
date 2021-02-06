06c1ded76324ee36cf56359dd0ead63b263a7c92
Julia <nuwanda57@yandex.ru>
2021-02-06 23:46:12 +0300

    tt

--- formulas_vae_v4/model.py
+++ formulas_vae_v4/model.py
@@ -194,7 +194,7 @@ class FormulaVARE(nn.Module):
         if unique:
             reconstructed_formulas = np.unique(reconstructed_formulas)
         print(reconstructed_formulas)
-        self.maybe_write_formulas([' '.join(f) for f in reconstructed_formulas], zs, out_file)
+        self.maybe_write_formulas(reconstructed_formulas, zs, out_file)
 
         return reconstructed_formulas, zs
 
