15edd9612f75725a08bb3d27a6430745b7a2e235
Julia <nuwanda57@yandex.ru>
2021-02-10 02:41:53 +0300

    tt

--- formulas_vae_v4/formula_utils.py
+++ formulas_vae_v4/formula_utils.py
@@ -3,6 +3,25 @@ import formulas_vae_v4.formula_config as my_formula_config
 from collections import deque
 
 
+def clear_redundant_operations(polish_formula_prefix):
+    tail_number_count = 0
+    while tail_number_count < len(polish_formula_prefix) and \
+            polish_formula_prefix[len(polish_formula_prefix) - 1 - tail_number_count] == my_formula_config.NUMBER_SYMBOL:
+        tail_number_count += 1
+
+    if tail_number_count == 0:
+        return
+
+    if tail_number_count < len(polish_formula_prefix) and \
+            polish_formula_prefix[len(polish_formula_prefix) - 1 - tail_number_count] in my_formula_config.OPERATORS:
+        operator_name = polish_formula_prefix[len(polish_formula_prefix) - 1 - tail_number_count]
+        if my_formula_config.OPERATORS[operator_name].arity == tail_number_count:
+            for _ in range(tail_number_count + 1):
+                polish_formula_prefix.pop()
+            polish_formula_prefix.append(my_formula_config.NUMBER_SYMBOL)
+            clear_redundant_operations(polish_formula_prefix)
+
+
 def maybe_get_valid(polish_formula):
     numbers_required = 1
     valid_polish_formula = []
@@ -15,6 +34,7 @@ def maybe_get_valid(polish_formula):
             numbers_required += (my_formula_config.OPERATORS[token].arity - 1)
         else:
             valid_polish_formula.append(token)
+            clear_redundant_operations(valid_polish_formula)
             numbers_required -= 1
             if numbers_required == 0:
                 return valid_polish_formula
@@ -56,12 +76,11 @@ if __name__ == '__main__':
     assert maybe_get_valid(['mult', 'add', 'sin', '5', 'cos', '6', 'add', '4', '3', 'add', '3', '1']) \
            == ['mult', 'add', 'sin', '5', 'cos', '6', 'add', '4', '3']
 
-    # print(get_formula_representation(['mult', 'add', '1', 'x', 'add', 'sin', '5', 'cos', '6']))
-    # print(get_formula_representation('sin sin sin x'.split()))
-    #
-    # print(get_formula_representation('mult mult x x mult x x'.split()))
-    # print(get_formula_representation('mult mult x x mult x x'.split()))
-    # print(get_formula_representation('mult mult x x mult x x'.split()))
-    # print(get_formula_representation('mult mult x x mult x x'.split()))
-    # print(get_formula_representation('mult mult x x mult x x'.split()))
-    # print(get_formula_representation('mult mult x x mult x x'.split()))
+    assert maybe_get_valid(['<n>']) == ['<n>']
+    assert maybe_get_valid(['<n>', '3']) == ['<n>']
+    assert maybe_get_valid(['mult', '<n>', '<n>', '<n>']) == ['<n>']
+    assert maybe_get_valid(['sin', '<n>']) == ['<n>']
+    assert maybe_get_valid(['sin', 'cos', 'add', '<n>', '<n>']) == ['<n>']
+    assert maybe_get_valid(['mult', 'add', 'sin', '<n>', 'cos', '6', 'add', '4', 'add', '<n>', '<n>']) \
+           == ['mult', 'add', '<n>', 'cos', '6', 'add', '4', '<n>']
+
