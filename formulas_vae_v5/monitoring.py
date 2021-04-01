58c05103afc52845e4f5c0dc2ad8d94b239cacc9
Julia <nuwanda57@yandex.ru>
2021-04-01 20:53:08 +0300

    active part

--- formulas_vae_v5/monitoring.py
+++ formulas_vae_v5/monitoring.py
@@ -3,14 +3,14 @@ import wandb
 
 class Monitoring:
     def __init__(self, project_name, name, correct_formula, x_range, experiment_config):
-        # wandb.init(project=project_name, name=name)
+        wandb.init(project=project_name, name=name)
         table = wandb.Table(columns=['correct formula', 'x_range'])
         table.add_data(correct_formula, x_range)
-        # wandb.log({'formula settings': table})
+        wandb.log({'formula settings': table})
         table = wandb.Table(columns=[*sorted(experiment_config.keys())])
         table.add_data(*[experiment_config[k] for k in sorted(experiment_config.keys())])
-        # wandb.log({'model config': table})
+        wandb.log({'model config': table})
 
     def log(self, wandb_log):
-        # wandb.log(wandb_log)
-        pass
+        wandb.log(wandb_log)
+        # pass
